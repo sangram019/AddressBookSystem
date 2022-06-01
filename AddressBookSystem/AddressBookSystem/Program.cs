b6cd400d1953ec1d83c18744545e68aea0ab471b
Sangram <sangram15.behera@gmail.com>
2022-06-01 09:42:10 +0530

    ADD Contacts

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -9,11 +9,29 @@ namespace AddressBookSystem
         public static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Address Book Program");
+            bool end = true;
+            Console.WriteLine("SelectNumber\n1.Add Contact\n2.Display\n3.End Of Program");
             Contacts contact = new Contacts();
-            AddAddressBookSystem book = new AddAddressBookSystem();
-            book.AddContacts();
-            book.Display();
+            AddContact addContact = new AddContact();
+            while (end)
+            {
+                Console.WriteLine("choose program to exicute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addContact.CreateContact();
+                        break;
+                    case 2:
+                        addContact.Display();
+                        break;
+                    case 3:
+                        end = false;
+                        Console.WriteLine("Program Is Ended");
+                        break;
 
+                }
+            }
         }
     }
 }
