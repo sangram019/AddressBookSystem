56adbc5534c5b2437a7a02dba3d2c0de0ca1021e
Sangram <sangram15.behera@gmail.com>
2022-06-02 01:38:19 +0530

    DELETE A CONTACT

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -1,26 +1,42 @@
-﻿bool end = true;
-Console.WriteLine("SelectNumber\n1.Add Contact\n2.Display\n3.EditContact\n4.End Of Program");
-Contacts contact = new Contacts();
-Address addContact = new AddressBook();
-while (end)
-{
-    Console.WriteLine("choose program to exucute : ");
-    int option = Convert.ToInt32(Console.ReadLine());
-    switch (option)
-    {
-        case 1:
-            addContact.CreateContact();
-            break;
-        case 2:
-            addContact.Display();
-            break;
-        case 3:
-            addContact.EditContact();
-            break;
-        case 4:
-            end = false;
-            Console.WriteLine("Program Is Ended");
-            break;
+﻿using System;
+using AddressBook;
 
+namespace AddressBook
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome To Address Book Program");
+            bool end = true;
+            Console.WriteLine("SelectNumber\n1.Add Contact\n2.Display\n3.EditContact\n4.DeleteContact\n5.End Of Program");
+            Contacts contact = new Contacts();
+            AddressBook addContact = new AddressBook();
+            while (end)
+            {
+                Console.WriteLine("choose program to exucute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addContact.CreateContact();
+                        break;
+                    case 2:
+                        addContact.Display();
+                        break;
+                    case 3:
+                        addContact.EditContact();
+                        break;
+                    case 4:
+                        addContact.DeleteContact();
+                        break;
+                    case 5:
+                        end = false;
+                        Console.WriteLine("Program Is Ended");
+                        break;
+
+                }
+            }
+        }
     }
 }
